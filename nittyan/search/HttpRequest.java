ce20c7cb7311feb379387ef8094e7378ea022713
nittyan <kaziotore@gmail.com>
2014-01-11 17:51:32 +0900

    Add user-agent setting constructor

--- nittyan/search/HttpRequest.java
+++ nittyan/search/HttpRequest.java
@@ -5,17 +5,32 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 
 public class HttpRequest {
 
     private static final String TAG = "HttpRequest";
+    private final String userAgent;
+    
+    public HttpRequest() {
+    	this("");
+    }
+    
+    public HttpRequest(String userAgent) {
+    	this.userAgent = userAgent;
+    }
 
     public String doGet(String url) {
         DefaultHttpClient client = null;
         try {
             HttpGet get = new HttpGet(url);
             client = new DefaultHttpClient();
+            if (!this.userAgent.isEmpty()) {
+            	HttpParams params = client.getParams();
+            	params.setParameter(CoreProtocolPNames.USER_AGENT, this.userAgent);
+            }
             HttpResponse response = client.execute(get);
             int status = response.getStatusLine().getStatusCode();
             if (HttpStatus.SC_OK != status) {
